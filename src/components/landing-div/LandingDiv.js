3194ae0d1be3cb3983647b3155183b481b70eb60
Sayan Faraz <sayan96@hotmail.com>
2019-07-12 11:24:30 -0400

    Almost done adding landing-div stuff

--- src/components/landing-div/LandingDiv.js
+++ src/components/landing-div/LandingDiv.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import './LandingDiv.scss'
+
 import NavBar from '../nav-bar/NavBar';
 
 function LandingDiv() {
     return(
-        <NavBar></NavBar>
+        <div className="LandingDiv">
+            <NavBar></NavBar>
+            {/* <!--Description--> */}
+            <div class="row text-center landing-text"> 
+            {/* <!--header--> */}
+                Driving neurotech innovation at the University of Toronto
+            </div>
+            <div class="row text-center landing-subtext"> 
+            {/* <!--subtext--> */}
+                Supporting student-run research and development, providing resources for those interested in learning about neurotech, and sharing ideas with like-minded individuals
+            </div>
+
+            <div class="row landing-btn-div">
+                <a class="btn btn-primary landing-btn justify-content-center" href="join.html" role="button">Join us</a>
+            </div>
+        </div>
     )
 }
 
