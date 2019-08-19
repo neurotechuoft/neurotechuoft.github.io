76b7b6b781b58625a7813e1c907ff0b196a500ad
CafeSamosaBlue <49924564+CafeSamosaBlue@users.noreply.github.com>
2019-08-19 16:54:29 -0400

    finished team page

--- src/components/team-div/TeamDiv.js
+++ src/components/team-div/TeamDiv.js
@@ -3,6 +3,11 @@ import './TeamDiv.scss';
 
 import {Link} from 'react-router-dom';
 
+import Taha from '../../assets/team/taha_morshedzadeh.jpg';
+import Sayan from '../../assets/team/sayan_faraz.jpg';
+import Kota from '../../assets/team/kota_abe.jpg';
+import Avatar from '../../assets/team/Profile_avatar_placeholder_large.png';
+
 function TeamDiv() {
   return(
     <div className="team-div">
@@ -12,6 +17,64 @@ function TeamDiv() {
           (Want to be a part of our team?
           <Link to="/opportunities"><b> Apply here :)</b></Link> )
         </h3>
+      </div>
+
+      <div className="team-photos">
+
+        <div>
+          <h2>Taha Morshedzadeh</h2>
+          <h3>Vice-President, Operations</h3>
+          <img src={Taha} alt="Taha Morshedzadeh"/>
+        </div>
+
+        <div>
+          <h2>Sayan Faraz</h2>
+          <h3>President</h3>
+          <img src={Sayan} alt="Sayan Faraz"/>
+        </div>
+
+        <div>
+          <h2>Musa Talluzi</h2>
+          <h3>MindType Co-Lead</h3>
+          <img src={Avatar} alt="Photo coming soon"/>
+        </div>
+
+        <div>
+          <h2>Vandod Farhadi</h2>
+          <h3>Workshops Lead</h3>
+          <img src={Avatar} alt="Photo coming soon"/>
+        </div>
+
+        <div>
+          <h2>Ritwij Jamuar</h2>
+          <h3>Cortical Apps Lead</h3>
+          <img src={Avatar} alt="Photo coming soon"/>
+        </div>
+
+        <div>
+          <h2>Li Quan Soh</h2>
+          <h3>Marketing Director</h3>
+          <img src={Avatar} alt="Photo coming soon"/>
+        </div>
+
+        <div>
+          <h2>Soaad Hossain</h2>
+          <h3>Ideas Exchange Director</h3>
+          <img src={Avatar} alt="Photo coming soon"/>
+        </div>
+
+        <div>
+          <h2>Kota Abe</h2>
+          <h3>Sponsorship Director</h3>
+          <img src={Kota} alt="Kota Abe"/>
+        </div>
+
+        <div>
+          <h2>Jai Aggarwal</h2>
+          <h3>Sponsorship Director</h3>
+          <img src={Avatar} alt="Photo coming soon"/>
+        </div>
+
       </div>
 
     </div>
